368d0342a2b807111b2584b239e88310a8086556
Elivelton Fernando <eliveltonfoliveira@gmail.com>
2026-01-20 19:13:52 -0300

    Initializes Nuxt project structure
    Sets up the basic file structure and configuration for a new Nuxt project.
    
    This includes:
    - Adding a .gitignore file to exclude unnecessary files from version control
    - Creating an index page with a welcome component
    - Configuring ESLint for code linting
    - Configuring the Nuxt application with modules for accessibility, linting, fonts, icons, and image handling
    - Adding a package lock file to ensure consistent dependency versions
    - Adding a default robots.txt to disallow indexing
    - Creating a basic tsconfig.json

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/a11y',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image'
+  ]
+})
